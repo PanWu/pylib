d909e48f104c50c51dbc245b821022650f02901e
Pan Wu <panwu@uber.com>
2016-11-25 21:40:52 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# for tests
+import os
+os.environ['TESTING'] = '1'
+
+install_requires = []
+setup_requires=[
+    "numpy==1.11.2",
+    "scipy==0.18.1",
+    "pandas==0.19.1",
+    "scikit-learn==0.18.1",
+]
+tests_require=[]
+
+setup(
+    name="pylib",
+    version="0.1.0",
+    description='Python utility functions library',
+    author='Pan Wu',
+    author_email='ustcwupan@gmail.com',
+    url='https://github.com/PanWu/pylib',
+
+    packages=find_packages(),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+
+    zip_safe=False
+)
