3daf70708b94f04eb47fb556e5cadfc2cb9c8aa8
Pan Wu <panwu@uber.com>
2016-12-04 00:23:53 -0800

    add script to save import time

---
+++ pylib/startup.py
@@ -0,0 +1,12 @@
+# export PYTHONSTARTUP=/Users/panwu/Code/pylib/pylib/startup.py
+
+import datetime, os, sys
+print(" (imported datetime, os, sys) ")
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print(" (imported np, pd, plt) ")
+#import datetime, os, pprint, re, sys, time
+#print("(imported datetime, os, pprint, re, sys, time)")
